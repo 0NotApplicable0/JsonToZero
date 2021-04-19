9591db5529b1f5331192785ff0f73341ce7dbb8d
NA <na.intj@pm.me>
2021-04-18 22:01:05 -0400

    chore: init

---
+++ JsonFormat.cs
@@ -0,0 +1,33 @@
+﻿// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+public class Enabled
+{
+    public bool book { get; set; }
+    public bool scroll { get; set; }
+    public bool wands { get; set; }
+    public bool npcs { get; set; }
+    public bool dispensers { get; set; }
+    public bool commands { get; set; }
+    public bool treasure { get; set; }
+    public bool trades { get; set; }
+    public bool looting { get; set; }
+}
+
+public class BaseProperties
+{
+    public int speed_duration { get; set; }
+    public int speed_strength { get; set; }
+    public int jump_boost_duration { get; set; }
+    public int jump_boost_strength { get; set; }
+}
+
+public class Root
+{
+    public Enabled enabled { get; set; }
+    public string tier { get; set; }
+    public string element { get; set; }
+    public string type { get; set; }
+    public int cost { get; set; }
+    public int chargeup { get; set; }
+    public int cooldown { get; set; }
+    public BaseProperties base_properties { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace JsonToZero
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting App!");
+            
+            var fileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                Title = "Open Json Files",
+            };
+
+            using (fileDialog)
+            {
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (var file in fileDialog.FileNames)
+                    {
+                        var serializer = new JsonSerializer();
+
+                        using (var sr = new StreamReader(file))
+                        using (var jsonTextReader = new JsonTextReader(sr))
+                        {
+                            var json = serializer.Deserialize<Root>(jsonTextReader);
+                            sr.Close();
+                            json.cooldown = 0;
+
+                            var jsonData = JsonConvert.SerializeObject(json);  
+                            // var path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                            // var fileName = Path.Combine(path, "test.txt");
+                            File.WriteAllText(file, jsonData);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
